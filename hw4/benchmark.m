76bb99dce6ba8cf61040d4ed921f89b21e9450fa
leoi11 <leolei@berkeley.edu>
2014-03-11 00:02:41 -0700

    modified the way the script loops over the parameters

--- hw4/benchmark.m
+++ hw4/benchmark.m
@@ -8,7 +8,7 @@ ytrain = ytrain(perm);
 num_folds = 10;
 fold_size = floor(num_samples / 10);
 accuracy = zeros(num_folds,1);
-lambdas = [0.01];
+lambdas = [0.05];
 rhos = [0.001];
 useSDescent = true;
 
@@ -30,36 +30,34 @@ for t=3:3;
             disp('Preprocessing by binarizing matrix...\n');
         end
         for i=1:size(lambdas,2);
-            for j=1:size(rhos,2);
-                if useSDescent %if using stochastic gradient descent
-                    numIter = 3; %Actual number of iterations = numIter * number of training samples 
-                else
-                    numIter = 5000; %Exact number of iterations
-                end
-                accuracy = zeros(num_folds,1);
-                fprintf('lambda = %f, Stochastic = %d, Rho = %f, numIter = %d\n', lambdas(1,i), useSDescent, rhos(1,j), numIter);
-                for f=1:fold_size:num_samples;
-                    test_upperbound = min(f+fold_size-1, num_samples);
-                    xtest = Xtrain(f:test_upperbound,:);
-                    ytest = ytrain(f:test_upperbound);
-                    xtrain_fold = [Xtrain(1:f-1,:); Xtrain(test_upperbound+1:num_samples,:)];
-                    ytrain_fold = [ytrain(1:f-1);   ytrain(test_upperbound+1:num_samples)];
-                    betaRV = train(xtrain_fold, ytrain_fold, lambdas(1,i), useSDescent, rhos(1,j), numIter);
-                    if isempty(betaRV) == 0 %if it is empty it means we returned incorrectly/intendedly
-                        hits = 0;
-                        num_tests = length(ytest);
-                        for k=1:num_tests;
-                            if predictor(xtest(k,:), betaRV, 0) == ytest(k);
-                                hits = hits+1;
-                            end
-                        end
-                        fprintf(' --- Accuracy at fold %d: %f\n', floor(f/fold_size)+1, hits / num_tests);
-                        accuracy(floor(f/fold_size)+1) = hits / num_tests;
-                    end
-                    %break; %comment this out to get the result for all 10 folds.
-                end
-                fprintf('Cross-validation accuracy: %f\n', mean(accuracy));
+            if useSDescent %if using stochastic gradient descent
+                numIter = 3; %Actual number of iterations = numIter * number of training samples 
+            else
+                numIter = 5000; %Exact number of iterations
             end
+            accuracy = zeros(num_folds,1);
+            fprintf('lambda = %f, Stochastic = %d, Rho = %f, numIter = %d\n', lambdas(1,i), useSDescent, rhos(1,i), numIter);
+            for f=1:fold_size:num_samples;
+                test_upperbound = min(f+fold_size-1, num_samples);
+                xtest = Xtrain(f:test_upperbound,:);
+                ytest = ytrain(f:test_upperbound);
+                xtrain_fold = [Xtrain(1:f-1,:); Xtrain(test_upperbound+1:num_samples,:)];
+                ytrain_fold = [ytrain(1:f-1);   ytrain(test_upperbound+1:num_samples)];
+                betaRV = train(xtrain_fold, ytrain_fold, lambdas(1,i), useSDescent, rhos(1,i), numIter);
+                if isempty(betaRV) == 0 %if it is empty it means we returned incorrectly/intendedly
+                    hits = 0;
+                    num_tests = length(ytest);
+                    for k=1:num_tests;
+                        if predictor(xtest(k,:), betaRV, 0) == ytest(k);
+                            hits = hits+1;
+                        end
+                    end
+                    fprintf(' --- Accuracy at fold %d: %f\n', floor(f/fold_size)+1, hits / num_tests);
+                    accuracy(floor(f/fold_size)+1) = hits / num_tests;
+                end
+                %break; %comment this out to get the result for all 10 folds.
+            end
+            fprintf('Cross-validation accuracy: %f\n', mean(accuracy));
         end
     end
 end
