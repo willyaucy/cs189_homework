747f44523949bebcf8a93a51bd2ad5f7bf97c7a6
leoi11 <leolei@berkeley.edu>
2014-04-06 18:28:07 -0700

    fixed some bugs

--- hw5/dTree.m
+++ hw5/dTree.m
@@ -2,9 +2,18 @@ function result=dtree()
     load('spam.mat'); % loads Xtrain, ytrain, Xtest into the workspace
     Xtrain = double(Xtrain); % converts to doubles
     Xtrain = horzcat(Xtrain,ytrain); % combine the labels with the samples, with labels at last column
-    root = growTree(Xtrain);
+    root = growTree(Xtrain, 0);
     
-function node=growTree(root)
+function node=growTree(root, depth)
+    if depth > 8
+        node.left = 0;
+        node.right = 0;
+        node.attr = 0;
+        node.splitpoint = 0;
+        return;
+    end
+    disp(size(root))
+    disp(depth);
     numFeatures = size(root, 2)-1;
     nonSpams = root( root(:,numFeatures+1)==0, : ); % matrix of non spams
     spams = root( root(:,numFeatures+1)==1, : ); % matrix of spams
@@ -14,6 +23,8 @@ function node=growTree(root)
     previousEntropy = calculateEntropy(root); % initial entropy
     maxInfoGain = -Inf;
     bestAttr = 0;
+    bestLeftSubtree = [];
+    bestRightSubtree = [];
     for i=1:numFeatures
         leftSubtree = root( root(:,i)<attrMeans(i), : ); % data that belongs to left subtree
         rightSubtree = root( root(:,i)>=attrMeans(i), : ); % data that belongs to right subtree
@@ -21,23 +32,30 @@ function node=growTree(root)
         sizeRight = size(rightSubtree, 1);
         totalSize = sizeLeft+sizeRight;
         infoGain = previousEntropy - ( sizeLeft/totalSize * calculateEntropy(leftSubtree) + sizeRight/totalSize * calculateEntropy(rightSubtree) );
+        disp(['inside loop initial entropy: ' num2str(previousEntropy) ' infoGain: ' num2str(infoGain)]);
         if infoGain > maxInfoGain
+           disp(' found max ');
            bestAttr = i;
            maxInfoGain = infoGain;
            bestLeftSubtree = leftSubtree;
            bestRightSubtree = rightSubtree;
         end
     end
-    node.left = growTree(bestLeftSubtree);
-    node.right = growTree(bestRightSubtree);
+    node.left = growTree(bestLeftSubtree, depth+1);
+    node.right = growTree(bestRightSubtree, depth+1);
     node.attr = bestAttr;
     node.splitpoint = attrMeans(bestAttr);
     
 function entropy=calculateEntropy(tree)
-    numFeatures = size(tree, 2);
+    numFeatures = size(tree, 2)-1;
     nonSpams = tree( tree(:,numFeatures+1)==0, : ); % matrix of non spams
     spams = tree( tree(:,numFeatures+1)==1, : ); % matrix of spams
     numNonSpams = size(nonSpams, 1);
     numSpams = size(spams, 1);
     totalSize = numNonSpams + numSpams;
-    entropy = - numSpams/totalSize * log2(numSpams/totalSize) - numNonSpams/totalSize * log2(numNonSpams/totalSize);
+    if numSpams/totalSize == 0 || numNonSpams/totalSize == 0
+        entropy = 0;
+    else
+        entropy = - numSpams/totalSize * log2(numSpams/totalSize) - numNonSpams/totalSize * log2(numNonSpams/totalSize);
+    end
+    
