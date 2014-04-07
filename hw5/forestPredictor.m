c556a7cc29c31112e0a0b13e3906268a788dc1ea
leoi11 <leolei@berkeley.edu>
2014-04-06 23:25:30 -0700

    fix bugs

--- hw5/forestPredictor.m
+++ hw5/forestPredictor.m
@@ -1,10 +1,10 @@
 function label=forestPredictor(data, dTrees)
     counter = zeros(2,1);
     for i=1:size(dTrees,1)
-        label=spamOrHam(data, dTrees(i));
+        label=spamOrHam(data, dTrees(i).root)+1;
         counter(label) = counter(label)+1;
     end
-    if counter(1) >= counter(0)
+    if counter(2) >= counter(1)
         label = 1;
     else
         label = 0;
--- hw5/randomForest.m
+++ hw5/randomForest.m
@@ -1,10 +1,12 @@
 function dTrees=randomForest(XtrainWithLabels, depth)
     treeSize = 23;
-    dTrees = zeros(treeSize,1);
+    for i=1:treeSize
+        dTrees(i).root = 0;
+    end
     numSamples = size(XtrainWithLabels, 1);
     for i=1:treeSize
         numSamplesSubset = ceil(numSamples * 0.67);
         sampleIndices = ceil( rand(numSamplesSubset,1) .* numSamples );
         XtrainSubset = XtrainWithLabels(sampleIndices, :);
-        dTrees(i) = dTree(XtrainSubset, depth, true);
+        dTrees(i).root = dTree(XtrainSubset, depth, true);
     end
