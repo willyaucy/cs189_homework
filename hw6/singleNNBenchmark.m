6afee1d1bfe69ab6e12a9986eec3942e70769cd7
leoi11 <leolei@berkeley.edu>
2014-04-20 00:20:02 -0700

    added option to test train set or testing set

--- hw6/singleNNBenchmark.m
+++ hw6/singleNNBenchmark.m
@@ -1,11 +1,30 @@
 function singleNNBenchmark(crossEntropyOn)
     load('data/train_small.mat');
     load('data/test.mat');
-    data = test;
-    for i=1:size(train, 2)
-        fprintf('Set %d\n',i);
-        dataWithLabel = preprocessMNIST(train{i});
-        [W_list,B_list]= singleNN(dataWithLabel, crossEntropyOn);
+    testTrainingData = false;
+    if testTrainingData
+        for i=1:size(train, 2)
+            fprintf('Set %d\n',i);
+            dataWithLabel = preprocessMNIST(train{i});
+            [W_list,B_list]= singleNN(dataWithLabel, crossEntropyOn);
+            numPoints = size(W_list, 3);
+            accuracies = size(numPoints, 1);
+            totalLoss = size(numPoints, 1);
+            for j=1:numPoints
+                [accuracies(j), totalLoss(j)] = singleNNPredictor(W_list(:,:,j), B_list(:,j), dataWithLabel);
+            end
+            scrsz = get(0,'ScreenSize');
+            figure('Position',[scrsz(1) scrsz(2) scrsz(3) scrsz(4)]);
+            subplot(1,2,1);
+            plot(accuracies*100, '-xr');
+            title(['Classification Accuracies on Testing Set ' num2str(i)]);
+            subplot(1,2,2);
+            plot(totalLoss, '-xb');
+            title(['Total Training Error on Training Set ' num2str(i)]);
+        end
+    else
+        dataWithLabel = preprocessMNIST(test);
+        [W_list,B_list]= singleNN(dataWithLabel);
         numPoints = size(W_list, 3);
         accuracies = size(numPoints, 1);
         totalLoss = size(numPoints, 1);
@@ -16,10 +35,10 @@ function singleNNBenchmark(crossEntropyOn)
         figure('Position',[scrsz(1) scrsz(2) scrsz(3) scrsz(4)]);
         subplot(1,2,1);
         plot(accuracies*100, '-xr');
-        title(['Classification Accuracies on Testing Set ' num2str(i)]);
+        title('Classification Accuracies on Testing Set');
         subplot(1,2,2);
         plot(totalLoss, '-xb');
-        title(['Total Training Error on Training Set ' num2str(i)]);
+        title('Total Training Error on Training Set');
     end
     
 function dataWithLabel=preprocessMNIST(dataSet)
