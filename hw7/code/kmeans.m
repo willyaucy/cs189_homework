e1d46d77d9c39a61073dda049c1f2de7dc6faaf5
leoi11 <leolei@berkeley.edu>
2014-05-02 00:20:07 -0700

    infinite while loop

--- hw7/code/kmeans.m
+++ hw7/code/kmeans.m
@@ -5,17 +5,17 @@ function means = kmeans(data, k)
     minValues = min(data(:,1:numFeatures), 1);
     means = rand(k, numFeatures) .* repmat(maxValues, k, 1) + repmat(minValues, k, 1); %initialize means (random choice)
     prevMean = zeros(k, numFeatures);
-    counts = zeros(1,k);
+    counts = zeros(k,1);
     while ~isequal(prevMean, means)
         prevMean = means;
         c = zeros(k, numFeatures); %a set of points belonging to cluster i, where 1 < i < k
         for j=1:numData
             pt = data(j, 1:numFeatures);
             pts = repmat(pt, k, 1); %a col vector of replicated points
-            l2Distances = pdist([pts means]);
+            l2Distances = norm(pts - means, 2);
             [val, index] = min(l2Distances);
             c(index, :) = c(index, :) + pt;
-            counts(1,index) = counts(1,index) + 1;
+            counts(index) = counts(index) + 1;
         end
-        means = c ./ counts;
+        means = c ./ repmat(counts, 1, numFeatures);
     end
