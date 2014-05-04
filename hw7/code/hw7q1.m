9e29eb05a9caf824a4c390e18dba8383bd6e4cdc
Alvin <alwong8@berkeley.edu>
2014-05-04 15:39:28 -0700

    fix pic size

--- hw7/code/hw7q1.m
+++ hw7/code/hw7q1.m
@@ -1,18 +1,20 @@
 function result = hw7q1()
     t = load('../data/train_small.mat');
     k = [5 10 20];
-    data = preprocessMNIST(t.train{7});
+    data = preprocessMNIST(t.train{1});
     for i=1:size(k, 2)
         disp(['Clustering with ' num2str(k(i)) ' means']);
         means = kmeans(data, k(i));
         scrsz = get(0,'ScreenSize');
-        figure('Position',[scrsz(1) scrsz(2) scrsz(3) scrsz(4)]);
-        for j=1:k(i)
-            %disp(means(j));
-            subplot(ceil(k(i)/2),2,j);
-            imagesc(reshape(means(j,:), sqrt(numel(means(j,:))), sqrt(numel(means(j,:)))));
-            title(['Mean ' j]);
-            colorbar;
+        for m=1:(k(i)/5)
+            figure('Position',[scrsz(1) scrsz(2) scrsz(3) scrsz(4)]);
+            for j=1:5
+                %disp(means(j+5*(m-1)));
+                subplot(ceil(5/2),2,j);
+                imagesc(reshape(means(j+5*(m-1),:), sqrt(numel(means(j+5*(m-1),:))), sqrt(numel(means(j+5*(m-1),:)))));
+                title(['Mean ' num2str(j+5*(m-1))]);
+                colorbar;
+            end
         end
     end
 
