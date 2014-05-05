2ba5489748bc58d09821b33159eb3f60e54d0a5f
Alvin <alwong8@berkeley.edu>
2014-05-05 14:10:01 -0700

    more eigfaces

--- hw7/code/hw7q2a.m
+++ hw7/code/hw7q2a.m
@@ -4,13 +4,17 @@ function hw7q2a()
 	data = preprocessCelebrityData(unmasked_pixels); % 158 x 17317
 	mean_data = data - repmat(mean(data,1), size(data,1), 1);
 	[u,s,v] = svd(mean_data', 0); %econ size
+	figure('Position',[100 100 1500 300]);
 	for i=1:10
-		eigenvector = u(:,i)
+		eigenvector = u(:,i);
 		full_im = zeros(size(mask(:,:,1)));
 		full_im(unmasked_pixels) = normalize_vec(eigenvector);
-		figure;
+		subplot(1,5,mod(i-1,5)+1);
 		imshow(full_im);
 		colormap(gray);
+		if i==5
+			figure('Position',[100 100 1500 300]);
+		end
 	end
 
 function vec = normalize_vec(vec)
