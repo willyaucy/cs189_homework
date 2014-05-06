557031c0119e857db510caa5ff6c927229c860c3
Alvin <alwong8@berkeley.edu>
2014-05-05 18:55:52 -0700

    fix?

--- hw7/code/preprocessCelebrityData.m
+++ hw7/code/preprocessCelebrityData.m
@@ -1,8 +1,12 @@
 function data=preprocessCelebrityData(unmasked_pixels)
 	data = [];
 	filenames = ls('../CelebrityDatabase');
+	if size(filenames, 1) == 1 %unix computer
+		filenamelist = regexp(filenames, '\t', 'split');
+		filenames = filenamelist';
+	end
 	for i=1:size(filenames, 1)
-		filename = strtrim(filenames(i,:));
+		filename = strtrim(char(filenames(i,:)));
 		if size(filename,2) > 4 && isequal(filename(size(filename,2)-3: size(filename,2)), '.jpg')
 			path = strcat('../CelebrityDatabase/', filename);
 			colorimg = imread(path);
