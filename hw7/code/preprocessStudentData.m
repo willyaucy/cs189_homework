4720d5cd510b0afff5af91ac0885635f1ed0b933
YAUYIN <yauyinx@gmail.com>
2014-05-05 19:24:05 -0700

    that's why people use octave

--- hw7/code/preprocessStudentData.m
+++ hw7/code/preprocessStudentData.m
@@ -5,7 +5,7 @@ function data=preprocessStudentData(unmasked_pixels)
         filenames = getAllFiles('../StudentDatabase');
 	end
 	for i=1:size(filenames, 1)
-		filename = strtrim(filenames(i,:));
+		filename = strtrim(char(filenames(i,:)));
 		if size(filename,2) > 4 && isequal(filename(size(filename,2)-3: size(filename,2)), '.jpg')
 			path = strcat('../StudentDatabase/', filename);
 			colorimg = imread(path);
